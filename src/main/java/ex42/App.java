4921967de4766fa9ded13b26bafa5149bf50b532
Khoa <khoa150899@gmail.com>
2021-06-16 23:20:13 -0400

    3rd Commit

--- src/main/java/ex42/App.java
+++ src/main/java/ex42/App.java
@@ -6,7 +6,6 @@ import java.util.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws IOException {
--- src/test/java/ex42/AppTest.java
+++ src/test/java/ex42/AppTest.java
@@ -10,23 +10,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
     @Test
     void fileTest() throws IOException {
-        String filePath = "C:\\Users\\khoa1\\IdeaProjects\\COP3330\\phan-cop3330-assignment3\\src\\main\\java\\ex42\\exercise42_input.txt";
-        ArrayList<Map<String, String>> nameList = file.read(filePath);
+        //original .txt file
+        String filePath1 = "C:\\Users\\khoa1\\IdeaProjects\\COP3330\\phan-cop3330-assignment3\\src\\main\\java\\ex42\\exercise42_input.txt";
+        ArrayList<Map<String, String>> nameList = file.read(filePath1);
         Collections.sort(nameList, new sortSalary());
 
-        StringBuilder expectedOutput = new StringBuilder();
-        expectedOutput.append(String.format("%-10s|%-10s|%-10s%n", "LAST", "FIRST", "SALARY"));
-        expectedOutput.append(String.format("----------|----------|----------%n"));
-        expectedOutput.append(String.format("%-10s|%-10s|$%-10s%n", "Swift", "Geoffrey", "$14200"));
-        expectedOutput.append(String.format("%-10s|%-10s|$%-10s%n", "Jones", "Chris", "$34500"));
-        expectedOutput.append(String.format("%-10s|%-10s|$%-10s%n", "Jones", "Aaron", "$46000"));
-        expectedOutput.append(String.format("%-10s|%-10s|$%-10s%n", "Zarnecki", "Sabrina", "$51500"));
-        expectedOutput.append(String.format("%-10s|%-10s|$%-10s%n", "Ling", "Mai", "$55900"));
-        expectedOutput.append(String.format("%-10s|%-10s|$%-10s%n", "Johnson", "Jim", "$56500"));
-        expectedOutput.append(String.format("%-10s|%-10s|$%-10s%n", "Xiong", "Fong", "$65000"));
+        //.txt file containing sorted values
+        String filePath2 = "C:\\Users\\khoa1\\IdeaProjects\\COP3330\\phan-cop3330-assignment3\\src\\main\\java\\ex42\\exercise42_test.txt";
+        ArrayList<Map<String, String>> expectedList = file.read(filePath2);
 
+        String expectedOutput = file.print(expectedList);
         String actualOutput = file.print(nameList);
 
-        assertEquals(expectedOutput.toString(), actualOutput);
+        assertEquals(expectedOutput, actualOutput);
     }
 }
