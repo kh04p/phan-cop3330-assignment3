c85b354b5d0b2d2faf9b1ae9824b60efe3ecc9de
Khoa <khoa150899@gmail.com>
2021-06-20 02:38:29 -0400

    7th Commit

---
+++ src/main/java/ex44/App.java
@@ -0,0 +1,23 @@
+package ex44;
+
+import java.util.Scanner;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        String filePath = "C:\\Users\\khoa1\\IdeaProjects\\COP3330\\phan-cop3330-assignment3\\src\\main\\java\\ex44\\exercise44_input.json";
+        Scanner in = new Scanner(System.in);
+        String user;
+
+        //list of maps of items read of JSON file will be created first.
+        ArrayList<Map<String, Object>> list =json.read(filePath);
+
+        //program will loop to ask user for product name until a product is found
+        do {
+            System.out.print("Enter a product name: ");
+            String name = in.nextLine().toLowerCase().trim(); //turns user input into lowercase string.
+            user = json.search(list, name); //passes user input and JSON list to search
+            System.out.println(user); //prints results
+        } while (user.equals("Product does not exist."));
+    }
+}
---
+++ src/main/java/ex44/json.java
@@ -0,0 +1,62 @@
+package ex44;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+
+public class json {
+    public static ArrayList<Map<String, Object>> read(String filePath) {
+        ArrayList<Map<String, Object>> returnList = new ArrayList<>();
+        File file = new File(filePath); //reads file location into a file first.
+
+        try {
+            JsonElement element = JsonParser.parseReader(new FileReader(file)); //parses JSON file into a JSON element
+            JsonObject object = element.getAsJsonObject(); //turns element into object to extract data
+
+            //since products is an array in JSON file, it'll need to be a JsonArray
+            JsonArray array = object.get("products").getAsJsonArray();
+
+            for (JsonElement productElement : array) { //loop through all items in JsonArray
+                JsonObject product = productElement.getAsJsonObject(); //turns parsed element into an object to extract data
+
+                //extracts data into different variables
+                String name = product.get("name").getAsString();
+                Double price = product.get("price").getAsDouble();
+                int quantity = product.get("quantity").getAsInt();
+
+                //due to there being different variable types, Maps will have to be <String, Object>
+                Map<String, Object> temp = new HashMap<>();
+                //adds extracted values into Map
+                temp.put("name", name);
+                temp.put("price", price);
+                temp.put("quantity", quantity);
+
+                //adds Map into list to return
+                returnList.add(temp);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to read JSON file. Try again.");
+        }
+
+        return returnList;
+    }
+
+    public static String search(ArrayList<Map<String, Object>> list, String name) {
+        for (Map<String, Object> temp : list) {
+            String lowercaseName = (String) temp.get("name"); //since equalsIgnoreCase is not working, a temporary String variable is needed.
+            if (lowercaseName.toLowerCase().equals(name)) {
+                //returns product details if name matches.
+                return String.format("Name: %s | Price: %.2f | Quantity: %d%n",
+                        temp.get("name"), temp.get("price"), temp.get("quantity"));
+            }
+        }
+        //returns error if product cannot be found.
+        return "Product does not exist.";
+    }
+}
